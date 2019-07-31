26e7d4c0a2e04361abf85795b67eee14b02833bd
sajrus33 <brianwala22@gmail.com>
2019-07-31 14:42:22 +0200

    lib is ready

--- testBar.js
+++ testBar.js
@@ -3,12 +3,15 @@ class testBar {
         this.parent = options.parent || document.body;
         this.position = options.position || "top";
         this.message = options.message || "default message";
+        this.call = options.call || (() => { alert("get widgets!") });
 
         const params = {
             self: {
                 style: {
                     position: "absolute",
-                    top: (this.position === "top") ? "0%" : "100%",
+                    top: (this.position === "top") ? "0%" : "",
+                    bottom: (this.position === "bottom") ? "0%" : "",
+
                     display: "flex",
                     alignItems: "center",
                     justifyContent: "center",
@@ -16,19 +19,18 @@ class testBar {
                     backgroundColor: "rgba(54,61,77,1)",
                     padding: "10px",
                     fontFamily: "Arial"
-
                 }
                 , txt: ""
+                , type: "div"
             },
             p: {
                 style: {
                     fontSize: "16px",
                     color: "#fff",
                     fontWeight: "500"
-
                 }
                 , txt: this.message
-
+                , type: "p"
             },
             btn: {
                 style: {
@@ -40,48 +42,60 @@ class testBar {
                     margin: "0 30px 0 10px",
                     fontWeight: "600",
                     whiteSpace: "nowrap"
-
                 }
                 , txt: "Get widgets"
-
-
+                , type: "button"
             },
             btnRemove: {
                 style: {
                     position: "absolute",
                     right: "5%",
-
                     backgroundColor: "transparent",
                     color: "#fff",
                     border: "none",
                 }
                 , txt: "X"
+                , type: "button"
             }
         };
-
-        this.self = document.createElement("div");
-        this.p = document.createElement("p");
-        this.btn = document.createElement("button");
-        this.btnRemove = document.createElement("button");
-
         const setStyles = (styles, element) => {
             Object.assign(element.style, styles);
         };
 
-        Object.keys(params).forEach(param => {
-            this[param] = document.createElement(param);
-            if (param !== "self") {
-                this.self.appendChild(this[param]);
-            } else if (param === "self") {
-                this.parent.appendChild(this.self);
-            }
-            this[param].innerText = params[param].txt;
-            setStyles(params[param].style, this[param]);
-        });
+        this.init = () => {
+            if (!this.initialized) {
+                this.initialized = true;
+                Object.keys(params).forEach(param => {
 
-        this.remove = () => {
-            this.self.remove();
-        }
+                    this[param] = document.createElement(params[param].type);
+                    if (param !== "self") {
+                        this.self.appendChild(this[param]);
+                    } else if (param === "self") {
+                        this.parent.appendChild(this.self);
+                    }
+                    this[param].innerText = params[param].txt;
+                    setStyles(params[param].style, this[param]);
+                });
+
+                this.self.animate([
+                    { transform: `translateY(calc(${this.position === "top" ? "100vh - 60px" : "-100vh + 60px"}))` },
+                    { transform: "translateY(0)" },
+                ],
+                    { duration: 600, easing: "ease-in-out" }
+                )
+
+                this.remove = () => {
+                    this.self.remove();
+                    this.initialized = false;
+                }
+
+                this.btnRemove.addEventListener("click", this.remove);
+                this.btn.addEventListener("click", this.call);
+            }
+        };
+
+
+
 
 
     }
